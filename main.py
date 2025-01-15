7ab09210ca851a8a80cc040a1cbdbb481f0d7ec0
Alvaro Riba <alvaroriba585@gmail.com>
2025-01-15 16:46:15 -0300

    stable 2.2

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ from tkinter import messagebox as MessageBox
 print('It is of utmost importance that you read the README.MD file before proceeding.\n\n\n')
 
 msg = input("Enter the message to be sent: ")
-cant = int(input("Enter the number of times you want to send the message: "))
-sleep(1)
-print('\n\nPlease switch the window to WhatsApp. \nIn 10 seconds messages will start to be sent.')
+cant = input("Enter the number of times you want to send the message: ")
 
-sleep(10)
+if cant.isdigit():
+    cant = int(cant)
+    sleep(1)
+    print('\n\nPlease switch the window to WhatsApp. \nIn 10 seconds messages will start to be sent.')
 
-for i in range(cant):
-    pg.write(msg)
-    pg.press('enter')
+    sleep(10)
 
-MessageBox.showinfo("Atention!", "The sending of messages has been completed.")
+    for i in range(cant):
+        pg.write(msg)
+        pg.press('enter')
+
+    MessageBox.showinfo("Atention!", "The sending of messages has been completed.")
+else:
+    print('\n\nWhat you have entered is not a number! You must enter ONLY a number.\nTry again.')
+
+
